26702df87aed41cb2ce1d20053d3d619b91afc7e
syedadnanalamin <syedadnanhuda64@gmail.com>
2026-06-21 21:34:29 +0600

    solved fetching error

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,17 @@
 
 // featured cards api
 
+
+// export const featuredCard = async () => {
+
+
+//     const data = await fetch("http://localhost:3000/data/tiles.json");
+//     const result = await data.json();
+//     return result;
+// }
+
+import tiles from "../../public/data/tiles.json";
+
 export const featuredCard = async () => {
-
-
-    const data = await fetch("http://localhost:3000/data/tiles.json");
-    const result = await data.json();
-    return result;
-}
+    return tiles;
+};
